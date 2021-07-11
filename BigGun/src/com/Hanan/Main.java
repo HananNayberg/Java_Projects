a4a6567403a1806317cbeed06d1297e3d435bee4
Hanan Nayberg <Nayberg.Hanan@gmail.com>
2021-07-11 17:28:27 -0600

    new BigGun project

---
+++ BigGun/src/com/Hanan/Main.java
@@ -0,0 +1,9 @@
+package com.Hanan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Big Gun AC DC");
+    }
+}
--- IronMan/src/Main.java
+++ IronMan/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("I AM IRON MAN");
+	System.out.println("Has He Lost His Mind? Can He See Or is He Blind?");
+	System.out.println("Nobody Likes Him ");
     }
 }
